03bf4771fd3a78acbeb3782656e52f38fa649285
Raina Kim <hrk296@nyu.edu>
2021-05-04 00:46:13 +0900

    merge

--- theme_calendar/user/models.py
+++ theme_calendar/user/models.py
@@ -4,8 +4,6 @@ class User(models.Model):
     name            = models.CharField(max_length = 50)
     email           = models.EmailField(max_length = 200, unique = True)
     password	    = models.CharField(max_length = 200, unique = True) 
-    profile_img_url = models.URLField(max_length = 2000, null = True)
-    created_at      = models.DateTimeField(auto_now_add = True, null = True)
 
     class Meta:
         db_table = 'users'
